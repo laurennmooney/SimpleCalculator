0d14207516e0aa24a2c7ab42c27bca43deac9c9e
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2020-01-08 00:18:47 -0500

    Add project files.

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumber(string inputAsString)
+        {
+            double convertedNumber;
+
+            if (!double.TryParse(inputAsString, out convertedNumber)) throw new ArgumentException("Expected a numeric value.");
+            return convertedNumber;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputConverter inputConverter = new InputConverter();
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+            Console.WriteLine("Please enter your first number");
+            double firstNumber = inputConverter.ConvertInputToNumber(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second number");
+            double secondNumber = inputConverter.ConvertInputToNumber(Console.ReadLine());
+
+            Console.WriteLine("Please enter the operation");
+            string operation = Console.ReadLine();
+
+            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+            Console.WriteLine($"Your answer is: {result}");
+        }
+
+    }
+}
